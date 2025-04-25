c5f5e5f18a0d77bc3e2fa8106bc5d154fe76df79
Marlon Villarama <marlonsvillarama@gmail.com>
2025-04-25 21:43:22 +0800

    20250425

--- src/components/BookingForm.svelte
+++ src/components/BookingForm.svelte
@@ -1,17 +1,43 @@
 <script>
     import Poster from '$lib/images/poster.jpg';
+    import FieldSet from './FieldSet.svelte';
 </script>
 
 <section class="wrapper">
-    <div class="form"></div>
-    <img src={Poster} alt="">
+    <aside>
+        <h4>How to book your appointment</h4>
+        <div class="steps">
+            <div class="step" data-form-step="1">
+                <span>Step 1: Choose what you want</span><br>
+                <p>Some text here...</p>
+            </div>
+            <div class="step active" data-form-step="2">
+                <span>Step 2: Share your details</span><br>
+                <p>Leave your contact details so we can confirm your appointment.</p>
+            </div>
+            <div class="step" data-form-step="3">
+                <span>Step 3: Confirm your appointment</span><br>
+                <p>Some text here...</p>
+            </div>
+        </div>
+    </aside>
+
+    <div class="form" data-form-step="1">
+        <FieldSet label="Your Name" />
+    </div>
+
+    <div class="form" data-form-step="2">
+    </div>
+
+    <div class="form" data-form-step="3">
+    </div>
 </section>
 
 <style>
     section {
         /* padding-top: 3rem; */
         /* padding-bottom: 3rem; */
-        border: 1px solid red;
+        /* border: 1px solid red; */
         display: flex;
         flex-direction: column;
         gap: 3rem;
@@ -19,43 +45,80 @@
     @media (min-width: 64rem) {
         section {
             display: grid;
-            grid-template-columns: calc(65% - 3rem) 35%;
-            gap: 3rem;
+            grid-template-columns: 40% calc(60% - 4rem);
+            gap: 4rem;
         }
     }
-    section > div {
+    /* .steps {
+        border: 1px solid red;
+    } */
+    aside h4 {
+        color: var(--color-grey-dark-03-rgb);
+        font-family: var(--font-serif);
+        font-size: var(--fs-xl);
+        font-weight: 700;
+        padding-bottom: 1.5rem;
+    }
+    .step {
+        border: 1px solid transparent;
+        padding: 1.5rem 1.75rem;
+        margin-bottom: 0.5rem;
+    }
+    .step.active {
+        border: 1px dotted var(--color-accent);
+        border-radius: 1rem;
+        box-shadow: var(--card-shadow);
+    }
+    .step > span {
+        color: var(--color-accent);
+        font-family: var(--font-default);
+        font-size: var(--fs-lg);
+        font-weight: 300;
+        display: inline-block;
+        letter-spacing: 0.25px;
+        padding-bottom: 0.5rem;
+    }
+    .step > p {
+        color: var(--color-grey-dark-03-rgb);
+        font-family: var(--font-default);
+        font-size: var(--fs-sm);
+        font-weight: 200;
+        padding-left: 2rem;
+        letter-spacing: 0.25px;
+    }
+    /* section > div {
         align-items: flex-start;
         justify-content: center;
-    }
-    section:first-child {
+    } */
+    /* section:first-child {
         display: none;
-    }
-    @media (min-width: 64rem) {
+    } */
+    /* @media (min-width: 64rem) {
         section:first-child {
             display: block;
             justify-content: flex-end;
             width: auto;
             align-items: right;
         }
-    }
+    } */
 
-    img {
+    /* img {
         display: none;
-    }
-    @media (min-width: 64rem) {
+    } */
+    /* @media (min-width: 64rem) {
         img {
             display: block;
-            /* max-inline-size: 100%; */
+            /!* max-inline-size: 100%; *!/
             width: 100%;
             block-size: auto;
             object-fit: contain;
             object-position: center center;
         }
-    }
+    } */
     .form {
         /* width: 50%; */
         /* min-width: 20rem; */
         /* margin: 0 auto; */
-        border: 1px solid green;
+        border: 0px solid green;
     }
 </style>
--- src/components/FieldSet.svelte
+++ src/components/FieldSet.svelte
@@ -1,11 +1,26 @@
-<script></script>
+<script>
+    import Dropdown from "./Dropdown.svelte";
+    let { label, help = '' } = $props();
+</script>
 
-<div class="fieldset"></div>
+<div class="fieldset">
+    <h4 class="label">{label}</h4>
+    {#if help}
+        <p class="help">{help}</p>
+    {/if}
+    <Dropdown id="test_dd" />
+    <span>test</span>
+</div>
 
 <style>
     .fieldset {
         min-width: 20rem;
-        width: 20rem;
-        border: 1px solid red;
+        width: 100%;
+        /* border: 1px solid red; */
+    }
+    h4 {
+        color: var(--color-grey-dark-03-rgb);
+        font-size: var(--fs-md);
+        font-weight: 400;
     }
 </style>
