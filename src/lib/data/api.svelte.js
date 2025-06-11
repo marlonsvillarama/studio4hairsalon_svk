79c42ea0699fc897d282f2c01e6da6d83e077904
Marlon Villarama <marlonsvillarama@gmail.com>
2025-06-11 19:54:13 +1200

    20250611

--- src/lib/data/api.svelte.js
+++ src/lib/data/api.svelte.js
@@ -6,3 +6,12 @@ export const API_URL = 'https://1j6rfkw7g4.execute-api.ap-southeast-2.amazonaws.
 // ns-1408.awsdns-48.org
 // ns-445.awsdns-55.com
 // ns-631.awsdns-14.net
+
+// d38dnu2h3h8uww.cloudfront.net
+// ns-1143.awsdns-14.org
+// ns-651.awsdns-17.net
+// ns-490.awsdns-61.com
+// ns-1729.awsdns-24.co.uk
+
+// Email users
+// appointments : m!2M*cFSL!EsdZb
